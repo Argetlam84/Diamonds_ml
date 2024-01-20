12ce8a377ab2feeb53a7f51691fde3f2de295fac
Mehmet Arslan <103227997+Argetlam84@users.noreply.github.com>
2024-01-20 21:46:03 +0300

    main.py

--- main.py
+++ main.py
@@ -12,27 +12,6 @@ import numpy as np
 
 df_d = sns.load_dataset("diamonds")
 
-"""
-Content
-price price in US dollars ($326--$18,823)
-
-carat weight of the diamond (0.2--5.01)
-
-cut quality of the cut (Fair, Good, Very Good, Premium, Ideal)
-
-color diamond colour, from J (worst) to D (best)
-
-clarity a measurement of how clear the diamond is (I1 (worst), SI2, SI1, VS2, VS1, VVS2, VVS1, IF (best))
-
-x length in mm (0--10.74)
-
-y width in mm (0--58.9)
-
-z depth in mm (0--31.8)
-
-depth total depth percentage = z / mean(x, y) = 2 * z / (x + y) (43--79)
-"""
-
 dr.check_data(df_d)
 
 cat_cols, num_cols, cat_but_car = dr.grab_col_names(df_d)
